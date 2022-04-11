7ca33a39694142e68bd405abaf0f5528acea61a3
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-04-10 22:28:30 -0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="contenedor">
                 <div className="redes-sociales" >
-                    <i className="bi bi-github"></i> 
+                    <i  className="bi bi-github"  ></i> 
                     <i className="bi bi-linkedin"></i> <div/>
                     
                     <h3>Jocelyn Rincón <i className="bi bi-dot"></i> 2022</h3>                </div>
